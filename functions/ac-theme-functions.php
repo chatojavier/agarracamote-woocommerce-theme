ed04110de5e5705a5cae34b9208ab90eb4c34e65
Javier Benavides <chato.javier@gmail.com>
2021-07-19 09:08:18 -0500

    Set max image resolution for upload.

--- functions/ac-theme-functions.php
+++ functions/ac-theme-functions.php
@@ -412,3 +412,38 @@ function storefront_do_shortcode( $tag, array $atts = array(), $content = null )
 
 	return call_user_func( $shortcode_tags[ $tag ], $atts, $content, $tag );
 }
+
+/** 
+ * Set max imagen resolution for media upload
+ */
+add_filter('wp_handle_upload_prefilter','ac_validate_image_size');
+function ac_validate_image_size( $file ) {
+    $image = getimagesize($file['tmp_name']);
+    $minimum = array(
+        'width' => '300',
+        'height' => '300'
+    );
+    $maximum = array(
+        'width' => '4500',
+        'height' => '4500'
+    );
+    $image_width = $image[0];
+    $image_height = $image[1];
+
+    $too_small = "Image dimensions are too small. Minimum size is {$minimum['width']} by {$minimum['height']} pixels. Uploaded image is $image_width by $image_height pixels.";
+    $too_large = "Image dimensions are too large. Maximum size is {$maximum['width']} by {$maximum['height']} pixels. Uploaded image is $image_width by $image_height pixels.";
+
+    if ( $image_width < $minimum['width'] || $image_height < $minimum['height'] ) {
+        // add in the field 'error' of the $file array the message 
+        $file['error'] = $too_small; 
+        return $file;
+    }
+    elseif ( $image_width > $maximum['width'] || $image_height > $maximum['height'] ) {
+        //add in the field 'error' of the $file array the message
+        $file['error'] = $too_large; 
+        return $file;
+    }
+    else
+        return $file;
+}
+?>
